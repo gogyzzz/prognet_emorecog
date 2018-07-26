7df4c36de57df021e2fb3ce717bb47ccaf19d9af
haeyong <gogyzzz@gmail.com>
2018-07-26 17:57:39 +0900

    added prognet.py

---
+++ prognet.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import pickle as pk
+import numpy as np
+
+def init_weights(m)
+
+class dnn(nn.Module):
+    def __init__(self, modelpath, on_cuda):
+        super(dnn, self).__init__()
+
+        # if model exists, load, message, exit
+
+        # else, init weight, etc.
+
+    def forward(self, x):
+
+
+
+
+
+class prognet(nn.Module):
+    def __init__(self, premodelpath, modelpath, on_cuda):
+
+        # if model exists, load, message, exit
+
+        # else, load premodel, init weight, etc.
+        
+
+        super(prognet, self).__init__()
+    def forward(self, x):
+
+    def getlayers(self, x):
+ 
+
+
