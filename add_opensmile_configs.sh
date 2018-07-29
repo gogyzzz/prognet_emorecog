7ce6c5662c9c9801146ce05afaa14e348f5fcd9f
haeyong <gogyzzz@gmail.com>
2018-07-29 16:32:36 +0900

    changed name of opensmile config files

--- add_opensmile_configs.sh
+++ add_opensmile_configs.sh
@@ -2,6 +2,5 @@
 
 osmdir=$1
 myconfdir="opensmile_configs"
-cp $myconfdir/standard_wave_input.conf.inc $osmdir/config/shared/.
-cp $myconfdir/wav_to_88egemaps_htk.conf.inc $osmdir/config/shared/.
-cp $myconfdir/eGeMAPSv01a.conf $osmdir/config/gemaps/.
+cp $myconfdir/*.conf.inc $osmdir/config/shared/
+cp $myconfdir/*.conf $osmdir/config/gemaps/
